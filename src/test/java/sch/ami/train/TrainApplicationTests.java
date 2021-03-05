eb391287465f2e9232d235063d5ef7bae6c8e23c
Amikuto <Amikuto@users.noreply.github.com>
2021-03-05 22:13:48 +0300

    1: Init

---
+++ src/test/java/sch/ami/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package sch.ami.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
